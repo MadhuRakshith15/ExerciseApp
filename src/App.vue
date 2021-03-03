74d8f6048a866642cfd12f2a7bae6ea84b165907
MadhuRakshith15 <madhurakshith15@gmail.com>
2021-03-03 17:00:28 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Exercise App"/>
+    <HelloWorld msg="Welcome to  App"/>
   </div>
 </template>
 
