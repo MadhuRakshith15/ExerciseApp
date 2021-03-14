4083d4eb724b8aa143706cb8e2da4b8e52bd2296
MadhuRakshith15 <madhurakshith15@gmail.com>
2021-03-14 19:40:27 -0400

    updated signup page

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,9 +1,33 @@
 <template>
     <div class="vue-template">
+     <nav class="navbar shadow bg-white rounded justify-content-between flex-nowrap flex-row fixed-top">
+      <div class="container">
+        <a class="navbar-brand float-left" href="https://myappr.herokuapp.com/" target="_blank">
+           Fitness Freak
+        </a>
+        <ul class="nav navbar-nav flex-row float-right">
+          <!-- <li class="nav-item">
+            <router-link class="nav-link pr-3" to="/workouts">Workouts</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link pr-5" to="/exercise">Exercises</router-link>
+          </li> -->
+          <li class="nav-item">
+            <router-link class="nav-link pr-3" to="/login">Sign in</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="btn btn-outline-primary mr-3" to="/signup">Sign up</router-link>
+          </li>
+          <!-- <li class="nav-item" style="">
+            <router-link class="btn btn-outline-info" to="/admin-login">Admin Login</router-link>
+          </li> -->
+        </ul>
+      </div>
+    </nav>
         <div class="inner-block">
-            <div class="vertical-center">
+            <div class="vertical-center pt-5">
         <form>
-            <h3>Sign Up</h3>
+            <h3 class="pt-5">Sign Up</h3>
 
             <div class="form-group">
                 <label>Full Name</label>
