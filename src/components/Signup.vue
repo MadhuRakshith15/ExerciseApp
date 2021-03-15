fdf63dd416eb4d17251d81c880524f95b8ac3258
MadhuRakshith15 <madhurakshith15@gmail.com>
2021-03-15 07:48:36 -0400

    signup fields updated

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -31,33 +31,75 @@
 
             <div class="form-group">
                 <label>Full Name</label>
-                <input type="text" class="form-control form-control-lg"/>
+                <!-- <input type="text" class=""/> -->
+                <input
+                  type="text"
+                  class="form-control form-control-lg"
+                  placeholder="Your Full name"
+                  required
+                  v-model.trim="user.fullname"
+                />
             </div>
 
             <div class="form-group">
                 <label>Email address</label>
-                <input type="email" class="form-control form-control-lg" />
+                <!-- <input type="email" class="form-control form-control-lg" /> -->
+                <input
+                type="text"
+                class="form-control form-control-lg "
+                placeholder="Email"
+                v-model.trim="user.username"
+                required
+              />
             </div>
 
             <div class="form-group">
                 <label>Password</label>
-                <input type="password" class="form-control form-control-lg" />
+                <input
+                type="password"
+                class="form-control form-control-lg"
+                placeholder="Your password"
+                v-model.trim="user.password"
+              />
+                
             </div>
 
             <div class="form-group">
                 <label>Age</label>
-                <input type="number" class="form-control form-control-lg" />
+                <input
+                type="number"
+                class="form-control form-control-lg"
+                placeholder="Age"
+                v-model.trim="user.age"
+              />
             </div>
 
             <div class="form-group">
                 <label>Weight(lbs)</label>
-                <input type="number" class="form-control form-control-lg" />
+                <input
+                type="number"
+                class="form-control form-control-lg"
+                placeholder="Weight"
+                v-model.trim="user.weight"
+              />
+                
             </div>
 
             <div class="form-group">
                 <label>Height"</label>
-                <input type="number" class="form-control form-control-lg" />
+                <input
+                type="number"
+                class="form-control form-control-lg"
+                placeholder="Weight"
+                v-model.trim="user.height"
+              />
             </div>
+
+            <div class="form-group">
+             <p class="help is-danger">
+            {{ error }}
+             </p>
+           </div>
 
             <button type="submit" class="btn btn-dark btn-lg btn-block">Sign Up</button>
 
@@ -72,9 +114,106 @@
 </template>
 
 <script>
-    export default {
-        data() {
-            return {}
-        }
+import router from "../router/index.js";
+export default {
+  name: "SignUp",
+  data() {
+    return {
+      user: {
+        username: "",
+        password: "",
+        fullname: "",
+        age: "",
+        weight: "",
+        height: ""
+      },
+      error: "",
+      picture: null,
+      notificationModal: {
+        isVisible: false,
+        modalColor: "is-success",
+        title: "",
+        text: "",
+      },
+      uploadProgressBar: {
+        isUploading: false,
+        uploadProgress: 0,
+      },
+    };
+  },
+  methods: {
+    validateFields() {
+      // username
+      if (this.user.username.length === 0) {
+        this.error = "Email should not be empty";
+        return false;
+      }
+      // firstname
+      if (this.user.fullname.length === 0) {
+        this.error = "fullname should not be empty";
+        return false;
+      }
+      // password
+      if (this.user.password.length === 0) {
+        this.error = "Password is required";
+        return false;
+      }
+        // lastname
+      if (this.user.age.length === 0) {
+        this.error = "age should not be empty";
+        return false;
+      }
+      if (this.user.weight.length === 0) {
+        this.error = "weight should not be empty";
+        return false;
+      }
+    if (this.user.height.length === 0) {
+        this.error = "height should not be empty";
+        return false;
+      }
+      return true;
+    },
+    //registers a new user
+    registerClicked() {
+      //  if fields are valid
+      if (this.validateFields()) {
+        //  get users in the session
+        let users = sessionStorage.users
+          ? JSON.parse(sessionStorage.getItem("users"))
+          : [];
+        users.push(this.user);
+        sessionStorage.setItem("users", JSON.stringify(users));
+        sessionStorage.setItem("user", JSON.stringify(this.user));
+        sessionStorage.setItem("isLoggedIn", true);
+        this.clearForm();
+        router.push("/");
+      }
+    },
+    clearForm() {
+      this.picture = null;
+      var userToClear = this.user;
+      userToClear.username = "";
+      userToClear.password = "";
+      userToClear.fullname = "";
+      userToClear.age = "";
+      userToClear.weight = "";
+      userToClear.height = "";
+    },
+  },
+  mounted() {
+    if (sessionStorage.isLoggedIn) {
+      this.$router.push("/user-dashboard");
     }
+  },
+};
 </script>
+
+<style scoped>
+.w-100,
+.field {
+  width: 100%;
+}
+.h-100perc {
+  height: 100%;
+}
+</style>
