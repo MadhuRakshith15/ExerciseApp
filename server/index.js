1a628c0b54cf3bacc994a8e3de6b01ef011b4248
MadhuRakshith15 <madhurakshith15@gmail.com>
2021-04-25 00:13:08 -0400

    update db credentials

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ const limiter = rateLimit({
 app.use(limiter);
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/rest-apis", { useNewUrlParser: true });
+mongoose.connect("mongodb://3.16.96.47:27017/rest-apis", { useNewUrlParser: true });
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
