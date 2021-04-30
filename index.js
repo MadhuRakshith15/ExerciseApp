55291a9fbcbd666a5e78a6f225e2c05d46e3f7b0
MadhuRakshith15 <madhurakshith15@gmail.com>
2021-04-29 21:59:49 -0400

    update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.get('*', (req, res) => {
   res.sendFile( path.join(__dirname, './server/public/index.html' ) );
 })
 
-app.listen(8000, () => {
+app.listen(8080, () => {
   console.log(`       
 ░█──░█ ░█▀▀▀ ░█─── ░█▀▀█ ░█▀▀▀█ ░█▀▄▀█ ░█▀▀▀ 　 
 ░█░█░█ ░█▀▀▀ ░█─── ░█─── ░█──░█ ░█░█░█ ░█▀▀▀ 　 
