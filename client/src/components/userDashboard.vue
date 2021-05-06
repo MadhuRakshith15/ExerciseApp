c379d6a195b076ccddbceb8259f59d4176752933
MadhuRakshith15 <madhurakshith15@gmail.com>
2021-05-06 14:26:33 -0400

    update files

--- client/src/components/userDashboard.vue
+++ client/src/components/userDashboard.vue
@@ -1,21 +1,54 @@
 <template>
 <div class="vue-template">
      <topnavbar />
-  <div>
-      <h1>Will catch up soon!!!</h1>  
-  </div>  
-  
-</div>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+     <div id="app">
+    <div style="margin:10% 25%; width: 250px;">
+      <br>
+      <ejs-autocomplete :dataSource='dataItem' :fields='dataFields'
+      placeholder="Select a name" popupWidth="250px" 
+      popupHeight="400px" :query='dataQuery' :highlight="true"> 
+      </ejs-autocomplete>
+    </div>
+ </div>
+ </div>
 
 </template>
 
+<script>
 
-<script type="text/javascript">
-  import topnavbar from './topnavbar.vue'
-  export default{
-    components:{
-      topnavbar // register component
-    }
+import Vue from "vue";
+import { AutoCompletePlugin } from "@syncfusion/ej2-vue-dropdowns";
+Vue.use(AutoCompletePlugin);
+import { DataManager, WebApiAdaptor,
+Query } from '@syncfusion/ej2-data';
+// import topnavbar from "./topnavbar.vue";
+
+export default Vue.extend({
+  data: function() {
+    return {
+       dataItem: new DataManager({
+         url: 'https://myappr.herokuapp.com/api/posts',
+         adaptor: new WebApiAdaptor,
+         crossDomain: true
+        }), 
+      dataFields:{ value: 'title' },
+      dataQuery: new Query().select(['title', 'description']).take(9).requiresCount(),
+      
+    };
   }
-
+}); 
 </script>
+
+
+<style>
+@import url(https://cdn.syncfusion.com/ej2/material.css);
+</style>
+
+
+
